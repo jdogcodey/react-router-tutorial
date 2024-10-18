957a364059a62c29933e4ead44af8961892669f8
Jonathan Dawson <jonnydawson@hotmail.com>
2024-10-18 12:37:36 +0100

    feat: Optimistic UI added to the favorite star

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,9 +1,16 @@
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from '../contacts';
+import { Form, useLoaderData, useFetcher } from "react-router-dom";
+import { getContact, updateContact } from '../contacts';
 
 export async function loader({ params }) {
   const contact = await getContact(params.contactId);
   return { contact };
+}
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  })
 }
 
 export default function Contact() {
@@ -72,9 +79,12 @@ console.log(contact.id)
 }
 
 function Favorite({ contact }) {
-  const favorite = contact.favorite;
+  const fetcher = useFetcher();
+
+  const favorite = fetcher.formData ? fetcher.formData.get("favorite") === "true" : contact.favorite;
+
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
@@ -86,6 +96,7 @@ function Favorite({ contact }) {
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
+
