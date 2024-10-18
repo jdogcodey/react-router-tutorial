07dc1b6339b01d22fdfe8f0519b8b7c2313e90bc
Jonathan Dawson <jonnydawson@hotmail.com>
2024-10-18 12:41:31 +0100

    Finish tutorial

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import * as ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
+  createRoutesFromElements,
+  Route,
 } from "react-router-dom";
 import "./index.css";
 import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
@@ -12,36 +14,37 @@ import EditContact, { action as editAction } from "./routes/edit";
 import { action as destroyAction } from './routes/destroy';
 import Index from './routes/index';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      { index: true, element: <Index /> },
-      {
-    path: "contacts/:contactId",
-    element: <Contact />,
-    loader: contactLoader,
-    action: contactAction,
-  },
-  {
-    path: 'contacts/:contactId/edit',
-    element: <EditContact />,
-    loader: contactLoader,
-    action: editAction,
-  },
-  {
-    path: 'contacts/:contactId/destroy',
-    action: destroyAction,
-    errorElement: <div>Oops! There was an error</div>
-  },
-    ],
-  },
-  
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Root />}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage />}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />} />
+        <Route
+          path="contacts/:contactId"
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path="contacts/:contactId/edit"
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route
+          path="contacts/:contactId/destroy"
+          action={destroyAction}
+        />
+      </Route>
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
