743277726923378f4390d1b4e23b9a5099cd5fd8
Jonathan Dawson <jonnydawson@hotmail.com>
2024-10-18 11:09:03 +0100

    Edit button properly wired up

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,8 @@ import {
 import "./index.css";
 import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import Contact, { loader as contactLoader } from "./routes/contact";
+import EditContact, { action as editAction } from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -19,8 +20,15 @@ const router = createBrowserRouter([
     children: [
       {
     path: "contacts/:contactId",
-    element: <Contact />
+    element: <Contact />,
+    loader: contactLoader,
   },
+  {
+    path: 'contacts/:contactId/edit',
+    element: <EditContact />,
+    loader: contactLoader,
+    action: editAction,
+  }
     ],
   },
   
