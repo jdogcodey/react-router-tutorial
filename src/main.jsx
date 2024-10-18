9128642525693f895dcd0ef8629e5ee0483b8fd4
Jonathan Dawson <jonnydawson@hotmail.com>
2024-10-18 10:30:54 +0100

    Change form to react router Form

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
 
@@ -14,6 +14,8 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
     children: [
       {
     path: "contacts/:contactId",
